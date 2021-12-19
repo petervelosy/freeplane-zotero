0fe958ae40fcc4b9bdf03ef024d28f0f653d861f
Peter Velosy <peter@petervelosy.com>
2021-12-19 06:54:33 +0100

    Create addon project structure

---
+++ src/main/groovy/Constants.groovy
@@ -0,0 +1,28 @@
+package com.petervelosy.freeplanezotero
+
+import okhttp3.MediaType
+
+class Constants {
+
+	public static final ZOTERO_CONNECTOR_URL = "http://127.0.0.1:23119/connector"
+	public static final EXEC_COMMAND_ENDPOINT = "/document/execCommand"
+	public static final RESPOND_ENDPOINT = "/document/respond"
+
+	public static final STORAGE_KEY_DOCUMENT_ID = "zotero_document_id"
+	public static final STORAGE_KEY_DOCUMENT_DATA = "zotero_document_data"
+
+	public static final NODE_ATTRIBUTE_CITATIONS = "zotero_citations"
+
+	public static final FIELD_CODE_PREFIX_CSL = "ITEM CSL_CITATION "
+
+	public static final ZOTERO_DIALOG_ICON_STOP = 0
+	public static final ZOTERO_DIALOG_ICON_NOTICE = 1
+	public static final ZOTERO_DIALOG_ICON_CAUTION = 2
+
+	public static final ZOTERO_DIALOG_BUTTONS_OK = 0
+	public static final ZOTERO_DIALOG_BUTTONS_OK_CANCEL = 1
+	public static final ZOTERO_DIALOG_BUTTONS_YES_NO = 2
+	public static final ZOTERO_DIALOG_BUTTONS_YES_NO_CANCEL = 3
+
+	public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+}
--- src/scripts/groovy/assignCitationsToNode.groovy
+++ src/scripts/groovy/assignCitationsToNode.groovy
@@ -2,36 +2,26 @@
 
 // TODO: create two menu items (only the first ExecutionModes gets parsed)
 // TODO: implement "Refresh all citations"
-// TODO: integrate the Freeplane Gradle plugin
 // TODO: Ability to transform links from online to offline
 // TODO: Only add a link automatically if no link exists or the link is a Zotero link
-// TODO: Set 'Show selected attributes only' as a default and hide zotero_ node attributes
 // TODO: Handle access control errors -> notify the user to set script permissions in Freeplane
 // TODO: Another integration scenario: create a semantic network (with labellable/classifiable connectors/edges) between many papers (extract references using e.g. https://github.com/CeON/CERMINE)
 // TODO: hierarchical node numbering?
 // TODO: copy citations from parent
+// TODO: sign scripts
 
-import groovy.transform.SourceURI
-import java.nio.file.Path
-import java.nio.file.Paths
+import com.petervelosy.freeplanezotero.Zotero
+import com.petervelosy.freeplanezotero.Constants
 
-@SourceURI
-URI sourceUri
+def zotero = new Zotero(ui, logger)
 
-Path scriptLocation = Paths.get(sourceUri)
-
-GroovyShell shell = new GroovyShell()
-def zotero = shell.parse(scriptLocation.resolveSibling('zotero.groovy').toFile())
-zotero.logger = logger
-zotero.ui = ui
-
-if (!zotero.getDocumentProperty(zotero.STORAGE_KEY_DOCUMENT_ID, node)) {
+if (!zotero.getDocumentProperty(Constants.STORAGE_KEY_DOCUMENT_ID, node)) {
   UUID uu = UUID.randomUUID()
-  node.mindMap.storage[zotero.STORAGE_KEY_DOCUMENT_ID] = uu.toString()
-  node.mindMap.storage[zotero.STORAGE_KEY_DOCUMENT_DATA] = ""
+  node.mindMap.storage[Constants.STORAGE_KEY_DOCUMENT_ID] = uu.toString()
+  node.mindMap.storage[Constants.STORAGE_KEY_DOCUMENT_DATA] = ""
   menuUtils.executeMenuItems(['ShowSelectedAttributesAction'])
 }
-logger.info("Starting with document ID ${zotero.getDocumentProperty(zotero.STORAGE_KEY_DOCUMENT_ID, node)}")
+logger.info("Starting with document ID ${zotero.getDocumentProperty(Constants.STORAGE_KEY_DOCUMENT_ID, node)}")
 logger.info("Document properties: ${zotero.propertiesToObj(node.mindMap.storage).toString()}")
 
 zotero.executeApiCommand('addEditCitation', node)
